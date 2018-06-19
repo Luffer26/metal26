b8834b635cbd7286e75eba467f9322484620d9cc
Nikita Skripko <nikitaskripko@mail.ru>
2018-06-19 23:24:41 +0300

    commit

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const Post = require("./routes/post")
 const bd = require("./bd.js")
 const app = express()
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static('build'));
@@ -24,17 +24,30 @@ app.post('/', function (req, res) {
         email: email,
         message: message
     })
+    res.redirect('back');
     return console.log("Отправлено")
 })
 
 app.get("/admin", function(req, res){
-    Post.find({}).then(posts => {
-        res.render('admin.ejs', {posts: posts})
-    })
+    res.redirect('/admin/login')    
 })
-app.post('/admin', function(req, res){
+app.post('/admin/authorized', function(req, res){
     const id = req.body._id
     Post.find({_id:  id}).remove().exec();
+    res.redirect('back');
+})
+app.get("/admin/login", function(req,res){
+    res.render("login.ejs")
+})
+app.post("/admin/login", function(req,res){
+    if(req.body.username == "admin" || req.body.password == "admin")(
+        res.redirect('/admin/authorized') 
+    )
+})
+app.get("/admin/authorized", function(req,res){
+    Post.find({}).then(posts => {
+        res.render('admin.ejs', {posts:posts})
+    })
 })
 
 module.exports = app
